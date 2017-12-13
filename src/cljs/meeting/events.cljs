d959e70442aebd29c3565b2e603649517808af4c
dosbol <dosbol.zhantolin@gmail.com>
2017-12-14 03:17:44 +0600

    beginning of create meeting

--- src/cljs/meeting/events.cljs
+++ src/cljs/meeting/events.cljs
@@ -2,6 +2,15 @@
   (:require [re-frame.core :as re-frame]
             [meeting.db :as db]))
 
+;;helper
+(defonce last-id (atom 0))
+
+(re-frame/reg-cofx
+  :meeting-id
+  (fn [cofx _]
+    (assoc cofx :meeting-id (swap! last-id inc))))
+
+;;init
 (re-frame/reg-event-db
  ::initialize-db
  (fn  [_ _]
@@ -26,3 +35,19 @@
  ::delete-meeting!
  (fn [db [_ id]]      ;;todo: filter meetings
    db))
+
+(re-frame/reg-event-fx
+ ::create-meeting!
+ [(re-frame/inject-cofx :meeting-id)]
+ (fn [cofx [_ new-meeting]]      
+   {:db (update-in (:db cofx) [:meetings] conj {(:meeting-id cofx) new-meeting})}))
+
+(re-frame/reg-event-fx 
+ ::set-hash!
+ (fn [cofx [_ location]]
+   {:change-loc location}))
+
+(re-frame/reg-fx
+ :change-loc
+  (fn [location]
+    (set! (.-hash js/window.location) location)))
--- src/cljs/meeting/views.cljs
+++ src/cljs/meeting/views.cljs
@@ -1,22 +1,38 @@
 (ns meeting.views
   (:require [re-frame.core :as re-frame]
+            [reagent.core  :as reagent]
             [meeting.subs :as subs]
+            [meeting.events :as events]
             ))
 
+(defn input-title
+  [{:keys [props]}]
+  (let [inner (reagent/atom "")]
+    (fn [] [:input (merge props
+                          {:type        "text"
+                           :value       @inner
+                           :on-change   #(reset! inner (-> % .-target .-value))})])))
 
 ;; home
 
 (defn home-panel []
     [:div (str "Hello. This is the Home Page.")
-     [:div [:a {:href "#/meetings"} "test #/meetings"]]
-     [:div [:a {:href "#/meetings/2"} "test #/meetings/2"]]])
+     [:div [:a {:href "#/meetings"} "create meeting"]]])
 
 
 ;; meeting
 
 (defn meeting-panel []
     [:div "This is the Meeting Page."
-    [:div [:a {:href "#/"} "go to Home Page"]]])
+    [:div [:a {:href "#/"} "go to Home Page"]]
+    [:form
+      [input-title {:props {:id "titlecreate"}}]
+      [:button {:type :button
+                :on-click #(do (re-frame/dispatch 
+                  [::events/create-meeting! {:title (.-value (.getElementById js/document "titlecreate"))}])
+                  (re-frame/dispatch [::events/set-active-panel :home-panel])
+                  (re-frame/dispatch [::events/set-hash! ""]))}
+              "create meeting"]]])
 
 
 ;; main
